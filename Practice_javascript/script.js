69fce899ec93bfe16b72d6fa1da3b10f4e19ea7e
SokhaHoun <houn.sokha566@gmail.com>
2024-12-28 14:44:21 +0700

    Add files via upload

---
+++ Practice_javascript/script.js
@@ -0,0 +1,10 @@
+
+document.querySelector("h1").textContent = "Hello, JavaScript!";
+
+
+document.querySelector("h2").style.color = "blue";
+
+
+const newItem = document.createElement("li");
+newItem.textContent = "Loops";
+document.querySelector("ul").appendChild(newItem);
